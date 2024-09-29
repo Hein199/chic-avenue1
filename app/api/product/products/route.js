e5c36c56c61dae83d387dd932c448423379bddf4
HeinNS <heinnaingsoe1999@gmail.com>
2024-09-30 03:16:15 +0700

    added cors origin

--- app/api/product/products/route.js
+++ app/api/product/products/route.js
@@ -1,17 +1,22 @@
 import Product from "@/models/Product";
 import { NextResponse } from "next/server";
 
-export async function GET(request) {
+export async function GET() {
     try {
         const products = await Product.find();
 
-        return NextResponse.json(
+        const response = NextResponse.json(
             {
                 message: "Products retrieved successfully!",
                 products: products,
             },
             { status: 200 }
         );
+        response.headers.set('Access-Control-Allow-Origin', '*'); // Allow all origins
+        response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Specify allowed methods
+        response.headers.set('Access-Control-Allow-Headers', 'Content-Type'); // Specify allowed headers
+
+        return response;
 
     } catch (error) {
         console.error("Error retrieving products:", error);
